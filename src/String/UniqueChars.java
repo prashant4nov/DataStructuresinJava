cbbb2da65c7135074058b989e44ae523355d2e9e
prashant kumar <jp@jp.local>
2016-05-23 18:20:14 -0500

    Data Structures in Java
    Data Structures in Java

---
+++ src/String/UniqueChars.java
@@ -0,0 +1,22 @@
+package String;
+
+/**
+ * Created by jp on 5/23/16.
+ */
+public class UniqueChars {
+    public static boolean isUniqueChars(String str) {
+        if (str.length()>256) return false;
+        boolean[] chars = new boolean[256];
+        for(int i=0;i<str.length();i++) {
+            int val = str.charAt(i);
+            if(chars[val]) return false;
+            chars[val]=true;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "abcde";
+        boolean bool = isUniqueChars(str);
+        System.out.println(bool);
+    }
+}
