6b49113bab4539fca4cc8a92a968869bc984becb
jp <prashant.talkin@gmail.com>
2016-05-24 01:53:38 -0500

    remove duplicates

--- src/String/RemoveDuplicateChars.java
+++ src/String/RemoveDuplicateChars.java
@@ -9,15 +9,15 @@ public class RemoveDuplicateChars {
     private static String removeDuplicates(String str) {
         if(str == null || str.length()<2) return null;
         char[] s = str.toCharArray();
-        boolean[] map = new boolean[256];
+        boolean[] mp = new boolean[256];
         for(int i=0;i<256;i++) {
-            map[i]=false;
+            mp[i]=false;
         }
-        map[s[0]]=true;
+        mp[s[0]]=true;
         int j=1;
         for (int i=1;i<s.length;i++) {
-            if(!map[s[i]]) {
-                map[s[i]]=true;
+            if(!mp[s[i]]) {
+                mp[s[i]]=true;
                 s[j]=s[i];
                 j++;
             }
