77a92ed1c9367eae03a5e3bfd580e4ff9bd3459a
jp <prashant.talkin@gmail.com>
2016-05-24 01:52:39 -0500

    remove duplicates

--- src/String/RemoveDuplicateChars.java
+++ src/String/RemoveDuplicateChars.java
@@ -1,11 +1,12 @@
 package String;
 
 /**
+ *
  * Design an algorithm and write code to remove the duplicate characters in a string.
  * Created by jp on 5/23/16.
  */
 public class RemoveDuplicateChars {
-    public static String removeDuplicates(String str) {
+    private static String removeDuplicates(String str) {
         if(str == null || str.length()<2) return null;
         char[] s = str.toCharArray();
         boolean[] map = new boolean[256];
@@ -15,7 +16,7 @@ public class RemoveDuplicateChars {
         map[s[0]]=true;
         int j=1;
         for (int i=1;i<s.length;i++) {
-            if(map[s[i]]==false) {
+            if(!map[s[i]]) {
                 map[s[i]]=true;
                 s[j]=s[i];
                 j++;
