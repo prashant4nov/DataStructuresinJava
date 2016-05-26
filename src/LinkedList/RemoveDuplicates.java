6f79e3e22a9a3693e1169ebec984f48c1a7f0dfa
jp <prashant.talkin@gmail.com>
2016-05-25 23:36:06 -0500

    input change

--- src/LinkedList/RemoveDuplicates.java
+++ src/LinkedList/RemoveDuplicates.java
@@ -6,53 +6,44 @@ import java.util.HashMap;
  * Created by jp on 5/25/16.
  */
 
- // Definition for singly-linked list.
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) { val = x; }
-}
-
-
 public class RemoveDuplicates {
 
     /**
-     * @param ListNode head is the head of the linked list
-     * @return: ListNode head of linked list
+     * @param Node head is the head of the linked list
+     * @return: Node head of linked list
      */
-    public static ListNode deleteDuplicates(ListNode head) {
+    public static Node deleteDuplicates(Node head) {
         if(head == null) return head;
         HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
-        ListNode curr = head;
-        map.put(curr.val, true);
+        Node curr = head;
+        map.put(curr.data, true);
         while(curr.next!=null) {
-            if(map.containsKey(curr.next.val)) {
+            if(map.containsKey(curr.next.data)) {
                 curr.next=curr.next.next;
             } else {
-                map.put(curr.next.val, true);
+                map.put(curr.next.data, true);
                 curr=curr.next;
             }
         }
         return head;
     }
 
-    public static void printList(ListNode node) {
-        ListNode n = node;
+    public static void printList(Node node) {
+        Node n = node;
         while(n != null) {
-            System.out.print(n.val+" ");
+            System.out.print(n.data+" ");
             n=n.next;
         }
     }
 
     public static void main(String[] args) {
-        ListNode list = new ListNode(1);
-        list.next = new ListNode(10);
-        list.next = new ListNode(12);
-        list.next.next = new ListNode(11);
-        list.next.next.next = new ListNode(11);
-        list.next.next.next.next = new ListNode(12);
-        list.next.next.next.next.next = new ListNode(11);
-        list.next.next.next.next.next.next = new ListNode(10);
+        Node list = new Node(1);
+        list.next = new Node(12);
+        list.next.next = new Node(11);
+        list.next.next.next = new Node(11);
+        list.next.next.next.next = new Node(12);
+        list.next.next.next.next.next = new Node(11);
+        list.next.next.next.next.next.next = new Node(10);
 
         System.out.println("Linked List before removing duplicates ");
         printList(list);
