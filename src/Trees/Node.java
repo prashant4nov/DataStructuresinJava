8ac46af22fd649e84d15fbb7c0e52b3fcffc7ac1
jp <prashant.talkin@gmail.com>
2016-05-26 02:37:28 -0500

    Node

---
+++ src/Trees/Node.java
@@ -0,0 +1,14 @@
+package Trees;
+
+/**
+ * Created by jp on 5/26/16.
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
