ca423c1d94f85a54c08d926832561947efb60211
jp <prashant.talkin@gmail.com>
2016-05-30 15:55:23 -0500

    Level Order Reverse

---
+++ src/Trees/LevelOrderReverse.java
@@ -0,0 +1,36 @@
+package Trees;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+/**
+ * Created by Prashant on 5/30/16.
+ */
+public class LevelOrderReverse {
+    static void levelOrderReverse(Node root) {
+        if(root == null) return;
+        Node node = root;
+        Stack<Node> stack = new Stack<Node>();
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(node);
+        Node tmp = null;
+        while(!queue.isEmpty()){
+            tmp = queue.poll();
+            stack.push(tmp);
+            if(tmp.right!=null) queue.add(tmp.right);
+            if(tmp.left!=null) queue.add(tmp.left);
+        }
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop().data+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        levelOrderReverse(root);
+    }
+}
