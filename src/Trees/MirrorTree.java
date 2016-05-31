b64d8e634c945100375cbfa882aabca419bdc586
jp <prashant.talkin@gmail.com>
2016-05-30 20:51:57 -0500

    Mirror Tree

---
+++ src/Trees/MirrorTree.java
@@ -0,0 +1,27 @@
+package Trees;
+
+/**
+ * Created by Prashant on 5/30/16.
+ */
+public class MirrorTree {
+    static void mirror(Node root) {
+        if(root==null) return;
+        mirror(root.left);
+        mirror(root.right);
+        Node tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+    }
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        a.left = new Node(2);
+        a.right = new Node(3);
+        a.left.left = new Node(4);
+        a.left.right = new Node(5);
+        a.right.left = new Node(6);
+        a.right.right = new Node(7);
+        TreeTraversals.inOrder(a);
+        mirror(a);
+        TreeTraversals.inOrder(a);
+    }
+}
