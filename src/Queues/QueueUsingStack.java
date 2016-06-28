7d09793919034a91cd1b054bea001d8eab30310c
jp <prashant.talkin@gmail.com>
2016-06-28 01:48:49 -0500

    TODO: queues

---
+++ src/Queues/QueueUsingStack.java
@@ -0,0 +1,11 @@
+package Queues;
+
+/**
+ * Created by Prashant on 6/28/16.
+ * http://www.geeksforgeeks.org/queue-using-stacks/
+ */
+public class QueueUsingStack {
+    public static void main(String[] args) {
+        System.out.print(" ");
+    }
+}
