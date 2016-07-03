ca0f928ff3f0e73c25f1bbd33fe9b831d4bc9d24
jp <prashant.talkin@gmail.com>
2016-07-02 23:41:51 -0500

    k sorted array.

--- src/Array/MergeKSortedArray.java
+++ src/Array/MergeKSortedArray.java
@@ -1,103 +1,25 @@
 package Array;
 
-/**
+import java.util.PriorityQueue;
+
+/**  Priority Queue is Heap in Java.
  * Created by Prashant on 7/2/16.
  * http://www.geeksforgeeks.org/merge-k-sorted-arrays/
  */
 
-class MinHeapNode{
-    int element;
-    int i;
-    int j;
-}
+class ArrayContainer implements Comparable<ArrayContainer>{
+    int[] a;
+    int index;
 
-class MinHeapModified {
-    int size;
-    int capacity;
-    MinHeapNode[] a;
-
-    MinHeapModified(int capacity) {
-        this.size = 0;
-        this.capacity = capacity;
-        this.a = new MinHeapNode[capacity];
+    public ArrayContainer(int[] a, int index) {
+        this.a = a;
+        this.index = index;
     }
 
-
-    public int getParent(int child) {
-        return (child-1)/2;
+    @Override
+    public int compareTo(ArrayContainer o) {
+        return this.a[this.index] - o.a[o.index];
     }
-
-    public int getLeftChild(int parent) {
-        return (2*parent+1);
-    }
-
-    public int getRightChild(int parent) {
-        return (2*parent+2);
-    }
-
-    public void insertKey(int key, int i, int j) {
-        if(size==capacity) {
-            System.out.println("Heap full!");
-            return;
-        }
-
-        size += 1;
-        a[size-1].element = key;
-        a[size-1].i = i;
-        a[size-1].j = j;
-        perculateUp(size-1);
-    }
-
-    public void perculateUp(int index) {
-        if(index==0) {
-            return;
-        }
-        int parent = getParent(index);
-        MinHeapNode tmp = a[index];
-        if(a[parent].element>a[index].element) {
-            a[index] = a[parent];
-            a[parent] = tmp;
-            perculateUp(parent);
-        }
-    }
-
-    public int getMin() {
-        return a[0].element;
-    }
-    public int extractMin() {
-        int min = a[0].element;
-        a[0] = a[size-1];
-        size--;
-        Heapify(0);
-
-        return min;
-    }
-
-    public void Heapify(int index) {
-        int leftChild = getLeftChild(index);
-        int rightChild = getRightChild(index);
-
-        int minIndex = index;
-        MinHeapNode minValue = a[index];
-
-        if(leftChild<size&&a[leftChild].element<a[minIndex].element) {
-            minIndex = leftChild;
-        }
-
-        if(rightChild<index&&a[rightChild].element<a[minIndex].element) {
-            minIndex = rightChild;
-        }
-
-        if(minIndex!=index) {
-            minValue = a[minIndex];
-            a[minIndex] = a[index];
-            a[index] = minValue;
-            Heapify(minIndex);
-        }
-    }
-
-
-
 }
 
 public class MergeKSortedArray {
@@ -105,12 +27,27 @@ public class MergeKSortedArray {
         int arr[][] = { {1, 3, 5, 7},
                 {2, 4, 6, 8},
                 {0, 9, 10, 11}} ;
-        int k = arr.length;
-        int n = arr[0].length;
-        MinHeap minHeap = new MinHeap(k);
-        for(int m=0;m<k;m++) {
-          //  minHeap.insertKey(arr[][0], i, j);
+
+        PriorityQueue<ArrayContainer> q = new PriorityQueue<ArrayContainer>();
+
+        int total = 0;
+        for(int i=0;i<arr.length;i++) {
+            q.add(new ArrayContainer(arr[i], 0));
+            total+=arr.length;
         }
+
+        int[] res = new int[total];
+
+        while(!q.isEmpty()) {
+            ArrayContainer ac = q.poll();
+
+            System.out.print(ac.a[ac.index] + ",");
+
+            if(ac.index < ac.a.length-1) {
+                q.add(new ArrayContainer(ac.a, ac.index+1));
+            }
+        }
+
         System.out.print(" ");
     }
 }
