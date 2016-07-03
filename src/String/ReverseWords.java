432bda087ae463159a9f2bee78d11d827a818128
jp <prashant.talkin@gmail.com>
2016-07-03 03:16:27 -0500

    Reverse Words in String

---
+++ src/String/ReverseWords.java
@@ -0,0 +1,28 @@
+package String;
+
+/**
+ * Created by Prashant on 7/3/16.
+ */
+public class ReverseWords {
+
+    public String reverseWords(String s) {
+        if(s==null||s.length()==0) return null;
+
+        String[] a = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = a.length-1;i>=0;i--) {
+            if(!a.equals(" ")) {
+                sb.append(a[i]).append(" ");
+            }
+
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String s = "hi how are you?";
+
+        ReverseWords rw = new ReverseWords();
+
+        System.out.print(rw.reverseWords(s));
+    }
+}
