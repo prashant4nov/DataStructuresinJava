9b0114d59ea1829fd2e64c512d8816b81157ed4f
prashant kumar <prashant4nov@gmail.com>
2017-02-01 21:22:01 -0500

    Update InplaceClockWise.java

--- src/Matrix/InplaceClockWise.java
+++ src/Matrix/InplaceClockWise.java
@@ -16,6 +16,19 @@ public class InplaceClockWise {
             }
         }
     }
+    
+    public static void antiClockWise(int[][] a) {
+        int n = a.length;
+        for(int x = 0; x < n/2; x++) {
+            for(int y = x; y < n-x-1; y++) {
+                int tmp = a[y][n-x-1];
+                a[y][n-x-1] = a[n-x-1][n-y-1];
+                a[n-x-1][n-y-1] = a[n-y-1][x];
+                a[n-y-1][x] = a[x][y];
+                a[x][y] = tmp;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int[] [] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
