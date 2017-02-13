f32980408f16809b78461cf8eaae049185ce4269
prashant kumar <prashant4nov@gmail.com>
2017-02-13 15:13:48 -0500

    Update LCS.java

--- src/DynamicProgramming/LCS.java
+++ src/DynamicProgramming/LCS.java
@@ -5,6 +5,12 @@ package DynamicProgramming;
  * http://www.geeksforgeeks.org/dynamic-programming-set-4-longest-common-subsequence/
  * O(mn) complexity
  */
+
+/**
+ * Created by Prashant on 6/25/16.
+ * http://www.geeksforgeeks.org/dynamic-programming-set-4-longest-common-subsequence/
+ * O(mn) complexity
+ */
 public class LCS {
     public int lcs(char[] a, char[] b) {
         int[][] tmp = new int[a.length+1][b.length+1];
@@ -22,12 +28,35 @@ public class LCS {
                 }
             }
         }
-        return max;
+        //return max;
+        int res = max;
+        char[] ls = new char[max];
+        int i = a.length, j = b.length;
+        
+        while(i > 0 && j > 0) {
+            if(a[i-1] == b[j-1]) {
+                ls[max-1] = a[i-1];
+                i--; j--; max--;
+            } else {
+              if(tmp[i-1][j] < tmp[i][j-1]) {
+                  j--;
+              } else {
+                  i--;
+              }
+            }
+        }
+        
+        for(Character c : ls) {
+            System.out.print(c);
+        }
+        return res;
     }
+
     public static void main(String[] args) {
         String str1 = "ABCDGHLQR";
         String str2 = "AEDPHR";
         LCS lcs = new LCS();
-        System.out.print(lcs.lcs(str1.toCharArray(), str2.toCharArray())+" ");
+        System.out.println(lcs.lcs(str1.toCharArray(), str2.toCharArray())+" ");
     }
 }
+
