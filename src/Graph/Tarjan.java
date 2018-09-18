3ffbcfb904a342d9409d296f3695ecfc4ac7db2a
prashant kumar <prashant4nov@gmail.com>
2018-09-18 02:55:27 -0500

    Update Tarjan.java

--- src/Graph/Tarjan.java
+++ src/Graph/Tarjan.java
@@ -1,34 +1,36 @@
 
 import static java.lang.Math.min;
-import java.util.*;
+import static java.lang.Math.max;
+import static java.lang.Math.toIntExact;
+
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class Tarjan {
-    static Scanner input;
-    private static ArrayList<Integer>[] adj;
-    private static int count, id;
+    private static Scanner input;
+    private static List<Integer>[] adj;
+    private static int scc, id;
     private static boolean[] onStack;
     private static int[] visited, lowLink;
     private static Stack<Integer> stack;
-    private static final int UNVISITED = -1;
 
 
     public static void tarjan() {
         id = 0;
-        count = 0;
+        scc = 0;
         visited = new int[adj.length];
         lowLink = new int[adj.length];
         onStack = new boolean[adj.length];
         stack = new Stack<Integer>();
 
         for(int i = 0; i < visited.length; i++) {
-            visited[i] = UNVISITED;
+            visited[i] = -1;
         }
 
         for(int i = 0; i < adj.length; i++)
-            if (visited[i] == UNVISITED)
+            if (visited[i] == -1)
                 dfs(i);
     }
 
@@ -40,7 +42,7 @@ public class Tarjan {
 
         for(int i = 0; i < adj[s].size(); i++) {
             int t = adj[s].get(i);
-            if (visited[t] == UNVISITED) {
+            if (visited[t] == -1) {
                 dfs(t);
             }
             if (onStack[t] == true) {
@@ -55,60 +57,120 @@ public class Tarjan {
                 lowLink[v] = visited[s];
                 if (v == s) break;
             }
-            count++;
+            scc++;
         }
 
     }
 
+    @SuppressWarnings("unchecked")
     public static void solve() {
-        int n = input.nextInt();
-        int m = input.nextInt();
-        adj = (ArrayList<Integer>[])new ArrayList[n];
+        int n = toIntExact(input.nextLong());
+        int m = toIntExact(input.nextLong());
+
+        if(n < 1 || n > 20000) {
+            System.out.println(0);
+            return;
+        }
+
+        if(m < 0 || m > 50000) {
+            System.out.println(0);
+            return;
+        }
+
+
+         adj = new ArrayList[toIntExact(n)];
 
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
         }
 
         for(int i = 0; i < m; i++) {
-            int s = input.nextInt() - 1;
-            int t = input.nextInt() - 1;
-            adj[s].add(t);
+            int s1 = toIntExact(input.nextLong());
+            if(s1 < 1) {
+                System.out.println(0);
+                return;
+            }
+            int s2 = toIntExact(input.nextLong());
+            if(s2 > n) {
+                System.out.println(0);
+                return;
+            }
+            if(s1 == s2) {
+                System.out.println(0);
+                return;
+            }
+
+            adj[--s1].add(--s2);
         }
 
         tarjan();
 
-        if(count == 1) {
+        if(scc <= 1) {
             System.out.println(0);
             return;
         }
 
 
-        int[] outdeg=new int[count];
-        int[] indeg=new int[count];
+        int[] out = new int[scc];
+        int[] in = new int[scc];
         for(int s = 0; s < n; s++) {
             for(int i = 0; i < adj[s].size(); i++) {
                 int t = adj[s].get(i);
-                if(visited[s]==visited[t]) continue;
-                outdeg[visited[s]]++;
-                indeg[visited[t]]++;
+                if(visited[s] != visited[t]) {
+                    out[visited[s]]++;
+                    in[visited[t]]++;
+                }
             }
         }
 
-        int nhead=0,ntail=0;
-        for(int i = 0; i < count; i++) {
-            if(outdeg[i]==0) ++ntail;
-            if(indeg[i]==0) ++nhead;
+        int result1 = 0;
+        int result2 = 0;
+        for(int i = 0; i < scc; i++) {
+            if(out[i] == 0)  {
+                result1++;
+            }
+            if(in[i] == 0) {
+                result2++;
+            }
         }
-        System.out.println(Math.max(nhead,ntail));
+        System.out.println(max(result1, result2));
 
     }
 
     public static void main(String[] arg) {
+        try {
+            input = new Scanner(System.in);
+            int x = toIntExact(input.nextLong());
+            if(x <= 100) {
+                for(int i = 0; i < x; i++) {
+                    solve();
+                }
+            } else {
+                System.out.println(0);
+            }
 
-        input = new Scanner(System.in);
-        int x = input.nextInt();
-        for(int i = 0; i < x; i++) {
-            solve();
+        } catch (Exception e) {
+            System.out.println(0);
         }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
